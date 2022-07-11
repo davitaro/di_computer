0cfa771a5eb36cc78b1b558b95e0d77bb019ecff
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-07-11 04:36:22 -0400

    connecting hierachy of modules and services via DI

---
+++ src/computer/computer.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
+
+@Controller('computer')
+export class ComputerController {
+  constructor(private cpuService: CpuService, private diskService: DiskService) {}
+}
